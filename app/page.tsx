5283274558d671f142dd6775688b539bdd322b25
Monisha Singh <monisha.singh06@gmail.com>
2026-02-05 17:21:58 +0530

    Project Display Section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Contact from "@/sections/Contact";
 import Footer from "@/sections/Footer";
 import Skills from "@/sections/Skills";
+import ProjectSection from "@/sections/ProjectDisplay";
 
 export default function Home() {
   return (
     <div>
+      <ProjectSection />
       <Skills />
       <Contact />
       <Footer />
